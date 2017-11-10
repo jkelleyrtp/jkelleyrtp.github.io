5eb981c5e89ee22058eb639c52e932b46daa8e7d
nuclearfusion2017 <kelley18j@ncssm.edu>
2017-11-10 13:15:29 -0500

    Eliminate roboto errors

--- js/main.js
+++ js/main.js
@@ -21,6 +21,8 @@ $(window).load(function() {
 
       if (scrollTop >= 100) {
         $('#switcher').addClass('scrolled-nav');
+
+        console.log(all_entires);
       } else if (scrollTop < 100) {
         $('#switcher').removeClass('scrolled-nav');
 
@@ -33,4 +35,21 @@ $(window).load(function() {
 
 
 
+});
+
+
+
+
+
+var all_entires = $.getJSON("https://spreadsheets.google.com/feeds/list/1691V8YfwWH3LAogJHUOsThCErwi-5qnY5a5sMGfIo7M/od6/public/values?alt=json", function(data) {
+  // All entries
+
+  var entries = data.feed.entry;
+
+  var projects = entries.filter(function(a){return a.gsx$type.$t =="Project"});
+
+  var activities = entries.filter(function(a){return a.gsx$type.$t =="Activities"});
+console.log(projects);
+return entries;
+  //console.log(data.feed)
 });
